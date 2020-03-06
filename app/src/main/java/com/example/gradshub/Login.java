f696123cb50dc412dd1874b09382fe58eed55459
bakang-dev <bakangmajova@gmail.com>
2020-03-06 20:36:57 +0200

    branch will be deleted after this commit (don't use)

--- app/src/main/java/com/example/gradshub/Login.java
+++ app/src/main/java/com/example/gradshub/Login.java
@@ -15,70 +15,68 @@ import org.json.JSONObject;
 
 public class Login extends AppCompatActivity {
 
+    User user = new User();
+    String userEmail, userPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button regButton = findViewById(R.id.registBtn);
-        regButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                registerActivity();
-            }
-        });
+        EditText emailET = findViewById(R.id.emailET);
+        EditText passwordET = findViewById(R.id.passwordET);
 
-        Button lecLoginBtn = findViewById(R.id.loginBtn);
-        lecLoginBtn.setOnClickListener(new View.OnClickListener() {
+        userEmail = emailET.getText().toString().trim();
+        userPassword = passwordET.getText().toString().trim();
+
+        Button loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
-
-                EditText emailET = findViewById(R.id.emailET);
-                EditText passwordET = findViewById(R.id.passwordET);
-
-                String userEmail = emailET.getText().toString();
-                String password = passwordET.getText().toString();
-
-                ContentValues params = new ContentValues();
-
-                params.put("USER_EMAIL", userEmail);
-                params.put("USER_PASSWORD", password);
-
-                AsyncHTTpPost asyncHttpPost = new AsyncHTTpPost(
-                        "https://gradshub.herokuapp.com/login.php", params) {
-                    @SuppressLint("StaticFieldLeak")
-                    @Override
-                    protected void onPostExecute(String output) {
-
-                        if (validate() == false) {
-                            Toast.makeText(Login.this, "One or more fields are missing!", Toast.LENGTH_SHORT).show();
-                        } else {
-                            //Toast.makeText(Login.this, "Testing ", Toast.LENGTH_SHORT).show();
-                            loginUser(output);
-                        }
-                    }
-
-                };
-                asyncHttpPost.execute();
+                requestLogin(userEmail, userPassword);
             }
         });
     }
 
-    private boolean validate() {
+
+    public void requestLogin(String userEmail, String userPassword) {
+        ContentValues params = new ContentValues();
+
+        params.put("USER_EMAIL", userEmail);
+        params.put("USER_PASSWORD", userPassword);
+
+        AsyncHTTpPost asyncHttpPost = new AsyncHTTpPost("https://gradshub.herokuapp.com/login.php", params) {
+            @SuppressLint("StaticFieldLeak")
+            @Override
+            protected void onPostExecute(String output) {
+
+                if (validateLoginInput() == false) {
+                    Toast.makeText(Login.this, "One or more fields are missing!", Toast.LENGTH_SHORT).show();
+                } else {
+                    requestResponse(output);
+                }
+            }
+
+        };
+        asyncHttpPost.execute();
+    }
+
+
+    public boolean validateLoginInput() {
 
         EditText emailET = findViewById(R.id.emailET);
         EditText passwordET = findViewById(R.id.passwordET);
 
-        String personNo = emailET.getText().toString().trim();
-        String password = passwordET.getText().toString().trim();
+        userEmail = emailET.getText().toString().trim();
+        userPassword = passwordET.getText().toString().trim();
 
-        if (personNo.isEmpty()) {
+        if (userEmail.isEmpty()) {
             emailET.setError("Field can't be empty!");
             emailET.requestFocus();
             return false;
         }
 
-        if (password.isEmpty()) {
+        if (userPassword.isEmpty()) {
             passwordET.setError("Field can't be empty!");
             passwordET.requestFocus();
             return false;
@@ -87,7 +85,8 @@ public class Login extends AppCompatActivity {
         return true;
     }
 
-    private void loginUser(String output) {
+
+    public void requestResponse(String output) {
 
         try {
 
@@ -97,6 +96,9 @@ public class Login extends AppCompatActivity {
 
             if (success.equals("1")) {
                 Toast.makeText(Login.this, message, Toast.LENGTH_SHORT).show();
+                //if the user entered the valid login credentials then we initialise the user login credentials.
+                user.setEmail(userEmail);
+                user.setPassword(userPassword);
                 feedActivity();
             }
             else if(success.equals("0")) {
@@ -114,9 +116,4 @@ public class Login extends AppCompatActivity {
         startActivity(intent);
     }
 
-
-    public void registerActivity() {
-        Intent intent = new Intent(this, Registration.class);
-        startActivity(intent);
-    }
 }
--- app/src/main/java/com/example/gradshub/Registration.java
+++ app/src/main/java/com/example/gradshub/Registration.java
@@ -1,135 +1,13 @@
 package com.example.gradshub;
 
-import android.annotation.SuppressLint;
-import android.content.ContentValues;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Toast;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class Registration extends AppCompatActivity {
-    //Naming the variables we used in this class, to avoid redefining(Making them global within this class).
-    private EditText nameET, surnameET, emailET, phoneNoET, passwordET;
-    private Spinner spinner;
-    private String userName, userSurname, phoneNum, userEmail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-
-        Button confButton = findViewById(R.id.confirmBtn);
-        confButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-
-                nameET = findViewById(R.id.nameEt);
-                surnameET = findViewById(R.id.surnameEt);
-                phoneNoET = findViewById(R.id.phoneNoEt);
-                emailET = findViewById(R.id.emailEt);
-                passwordET = findViewById(R.id.passEt);
-                spinner = findViewById(R.id.spinner);
-
-                userName = nameET.getText().toString();
-                userSurname = surnameET.getText().toString();
-                phoneNum = phoneNoET.getText().toString();
-                userEmail = emailET.getText().toString();
-                String password = passwordET.getText().toString();
-                //confirm password is defined and used in validate method only.
-                String levelOfStudy = spinner.getSelectedItem().toString();
-                ContentValues params = new ContentValues();
-
-
-
-                params.put("USER_NAME", userName);
-                params.put("USER_SURNAME", userSurname);
-                params.put("PHONE_NUMBER", phoneNum);
-                params.put("USER_EMAIL", userEmail);
-                params.put("USER_PASSWORD", password);
-                params.put("LEVEL_OF_STUDY", levelOfStudy);
-
-
-                //Todo: Match the parameters with the PHP file, and change the php url
-                @SuppressLint("StaticFieldLeak") AsyncHTTpPost asyncHttpPost = new AsyncHTTpPost(
-                        "https://gradshub.herokuapp.com/login.php", params) {
-                    @SuppressLint("StaticFieldLeak")
-                    @Override
-                    protected void onPostExecute(String output) {
-
-                        if (validate() == false) {
-                            Toast.makeText(Registration.this, "One or more fields are missing!", Toast.LENGTH_SHORT).show();
-                        } else {
-                            //Toast.makeText(Login.this, "Testing ", Toast.LENGTH_SHORT).show();
-                            registerUser(output);
-                        }
-                    }
-
-                };
-                asyncHttpPost.execute();
-            }
-        });
-
-    }
-
-    private boolean validate() {
-
-        EditText passwordET = findViewById(R.id.passwordET);
-        EditText confPass = findViewById(R.id.confirmPassEt);
-        String password = passwordET.getText().toString().trim();
-
-        if (userName.trim().isEmpty() || userSurname.trim().isEmpty() || phoneNum.trim().isEmpty() || userEmail.trim().isEmpty()) {
-            emailET.setError("Field can't be empty!");
-            emailET.requestFocus();
-            return false;
-        }
-        //ToDo:Validate the spinner, check if the year of study is selected.
-
-        if (password.isEmpty()) {
-            passwordET.setError("Field can't be empty!");
-            passwordET.requestFocus();
-            return false;
-        }
-        //validating password and password confirmation
-        if(!password.equals(confPass)){
-            confPass.setError("Please ensure you have entered correct password!");
-            confPass.requestFocus();
-            return false;
-        }
-
-
-        return true;
-    }
-
-    private void registerUser(String output) {
-
-        try {
-
-            JSONObject jo = new JSONObject(output);
-            String success = jo.getString("success");
-            String message =jo.getString("message");
-
-            if (success.equals("1")) {
-                Toast.makeText(Registration.this, message, Toast.LENGTH_SHORT).show();
-                feedActivity();
-            }
-            else if(success.equals("0")) {
-                Toast.makeText(Registration.this,message,Toast.LENGTH_SHORT).show();
-            }
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public void feedActivity() {
-        Intent intent = new Intent(this, Feed.class);
-        startActivity(intent);
     }
 }
