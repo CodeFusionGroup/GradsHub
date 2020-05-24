a4b6061c0ad7ac2c0b315731464e79c7aaa93caf
rekgotso <rekgotsonkwe@gmail.com>
2020-05-24 06:55:29 +0200

    Classes and Xmls for showing posts in cardview on the home page

--- app/src/main/java/com/example/gradshub/main/home/HomeFragment.java
+++ app/src/main/java/com/example/gradshub/main/home/HomeFragment.java
@@ -10,14 +10,23 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.gradshub.R;
+import com.example.gradshub.adapters.AdapterPosts;
+import com.example.gradshub.model.ModelPost;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 //TODO: implement functionality to display feeds on the home page in the form of card views.
 public class HomeFragment extends Fragment {
 
-
+    RecyclerView recyclerView;
+    List<ModelPost> postList;
+    AdapterPosts adapterPosts;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +35,33 @@ public class HomeFragment extends Fragment {
 
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        //recycler view  and its properties
+        recyclerView = view.findViewById(R.id.postsRecyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+//        layoutManager.setStackFromEnd(true);
+//        layoutManager.setReverseLayout(true);
+
+        recyclerView.setLayoutManager(layoutManager);
+
+        //init post list
+        //postList = new ArrayList<>();
+        postList = new ArrayList<>();
+
+        loadPosts();
+
+
+        return view;
+    }
+    /*This is where we need to load post from database using the ModelPost class*/
+    private void loadPosts() {
+        //path of all the posts
+        //get data from database and add it to postlist array
+        //postList.add();
+
+
     }
 
 
