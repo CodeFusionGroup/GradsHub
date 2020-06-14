2d748ee4c033938284195df1de3e92c56f292551
DrPhenomenon <maccauleymakgonye@gmail.com>
2020-06-14 22:38:58 +0200

    TestUpdates

---
+++ app/src/androidTest/java/com/example/gradshub/main/home/FeedListFragmentTest.java
@@ -0,0 +1,47 @@
+package com.example.gradshub.main.home;
+
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import androidx.test.rule.ActivityTestRule;
+
+import com.example.gradshub.R;
+import com.example.gradshub.main.creategroup.CreateGroupFragment;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.junit.Assert.*;
+
+public class FeedListFragmentTest {
+    @Rule
+    public ActivityTestRule<FeedListActivity> activityTestRule = new ActivityTestRule<FeedListActivity>(FeedListActivity.class);
+    private FeedListActivity mActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        mActivity = activityTestRule.getActivity();
+    }
+
+    @Test
+    public void Launched()
+    {
+        RelativeLayout rlContainer = (RelativeLayout) mActivity.findViewById(R.id.feedlist_container);
+        assertNotNull(rlContainer);
+        FeedListFragment Fragment = new FeedListFragment();
+        mActivity.getSupportFragmentManager().beginTransaction().add(rlContainer.getId(),Fragment).commitAllowingStateLoss();
+        getInstrumentation().waitForIdleSync();
+        View view = Fragment.getView().findViewById(R.id.feedList);
+        assertNotNull(view);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mActivity = null;
+
+        mActivity = null;
+    }
+}
---
+++ app/src/main/java/com/example/gradshub/main/home/FeedListActivity.java
@@ -0,0 +1,17 @@
+package com.example.gradshub.main.home;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.gradshub.R;
+
+public class FeedListActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle onSaveInstance)
+    {
+        super.onCreate(onSaveInstance);
+        setContentView(R.layout.feedlist_test);
+    }
+}
