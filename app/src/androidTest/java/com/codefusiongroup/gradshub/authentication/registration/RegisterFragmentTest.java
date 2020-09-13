bfb6b8b072b67ff857baf2975cc68f8477f1a644
cogeta <1682836@students.wits.ac.za>
2020-09-14 01:45:10 +0200

    TestUpdate:RegisterTest
    Changed screen resolution to work on  smaller screen resolutions

--- app/src/androidTest/java/com/codefusiongroup/gradshub/authentication/registration/RegisterFragmentTest.java
+++ app/src/androidTest/java/com/codefusiongroup/gradshub/authentication/registration/RegisterFragmentTest.java
@@ -7,6 +7,7 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasErrorText;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static com.codefusiongroup.gradshub.authentication.AuthenticationActivityTest.waitForResources;
 import static com.codefusiongroup.gradshub.common.AssisterMethods.logUserOut;
 import static org.hamcrest.Matchers.allOf;
 
@@ -45,10 +46,10 @@ public class RegisterFragmentTest {
         onView(withId(R.id.lastNameET)).perform(typeText("lname"));
         onView(withId(R.id.emailET)).perform(typeText("tester141414fhfvbd@gmail.com"));
         closeSoftKeyboard();
-        onView(withId(R.id.phoneNumberET)).perform(typeText(String.valueOf("123456789")));
+        onView(withId(R.id.phoneNumberET)).perform(scrollTo(), typeText(String.valueOf("123456789")));
         closeSoftKeyboard();
         onView(withId(R.id.spinner)).perform(click());
-        onView(allOf(withText("Honours"))).perform(click());
+        onView(allOf(withText("Honours"))).perform(scrollTo(), click());
         ViewInteraction view4 = onView(withId(R.id.passwordET));
         view4.perform(typeText("aBC123xyZ!"));
         closeSoftKeyboard();
@@ -62,14 +63,14 @@ public class RegisterFragmentTest {
 
     @Test
     public void testInvalidFirstName(){
-        onView(withId(R.id.submitBtn)).perform(click());
+        onView(withId(R.id.submitBtn)).perform(scrollTo(), click());
         onView(withId(R.id.firstNameET)).check(matches(hasErrorText(fieldError)));      //Correct error message was returned.
     }
 
     @Test
     public void testInvalidLastName(){
-        onView(withId(R.id.firstNameET)).perform(typeText("f_name"));
-        onView(withId(R.id.submitBtn)).perform(click());
+        onView(withId(R.id.firstNameET)).perform(typeText("f_name"), closeSoftKeyboard());
+        onView(withId(R.id.submitBtn)).perform(scrollTo(), click());
         onView(withId(R.id.lastNameET)).check(matches(hasErrorText(fieldError)));      //Correct error message was returned.
     }
 
@@ -78,21 +79,19 @@ public class RegisterFragmentTest {
         //first test is for no email was provided
         onView(withId(R.id.firstNameET)).perform(typeText("f_name"));
         onView(withId(R.id.lastNameET)).perform(typeText("lname"));
-        onView(withId(R.id.submitBtn)).perform(click());
-        onView(withId(R.id.emailET)).check(matches(hasErrorText(fieldError)));      //Correct error message was returned.
         //second test is for checking if correct error is shown when incorrect email is provided
-        onView(withId(R.id.emailET)).perform(typeText("tester141414fhfvbdcom"));
-        onView(withId(R.id.submitBtn)).perform(click());
+        onView(withId(R.id.emailET)).perform(typeText("tester141414fhfvbdcom"), closeSoftKeyboard());
+        onView(withId(R.id.submitBtn)).perform(scrollTo(), click());
         onView(withId(R.id.emailET)).check(matches(hasErrorText("check that your email address is entered correctly!")));      //Correct error message was returned.
     }
 
     @Test
-    public void testInvalidPhoneNumber(){
+    public void testInvalidPhoneNumber() throws InterruptedException {
         //Invalid phone number is provided.
-        onView(withId(R.id.firstNameET)).perform(typeText("f_name"));
-        onView(withId(R.id.lastNameET)).perform(typeText("lname"));
-        onView(withId(R.id.emailET)).perform(typeText("tester141414fhfvbd@gmail.com"));
-        closeSoftKeyboard();
+        onView(withId(R.id.firstNameET)).perform(typeText("f_name"), closeSoftKeyboard());
+        onView(withId(R.id.lastNameET)).perform(typeText("lname"), closeSoftKeyboard());
+        onView(withId(R.id.emailET)).perform(typeText("tester141414fhfvbd@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.submitBtn)).perform(scrollTo());
         onView(withId(R.id.submitBtn)).perform(click());
         onView(withId(R.id.phoneNumberET)).check(matches(hasErrorText(fieldError)));      //Correct error message was returned.
 
@@ -106,7 +105,7 @@ public class RegisterFragmentTest {
         closeSoftKeyboard();
         onView(withId(R.id.phoneNumberET)).perform(typeText(String.valueOf("123456789")));
         closeSoftKeyboard();
-        onView(withId(R.id.submitBtn)).perform(click());
+        onView(withId(R.id.submitBtn)).perform(scrollTo(), click());
 //        onView(withId(R.id.spinner)).check(matches(hasErrorText("Please select valid academic status!")));      //Correct error message was returned.
     }
 
@@ -120,7 +119,7 @@ public class RegisterFragmentTest {
         closeSoftKeyboard();
         onView(withId(R.id.spinner)).perform(click());
         onView(allOf(withText("Honours"))).perform(click());
-        onView(withId(R.id.submitBtn)).perform(click());
+        onView(withId(R.id.submitBtn)).perform(scrollTo(), click());
         onView(withId(R.id.passwordET)).check(matches(hasErrorText(fieldError)));      //Correct error message was returned.
     }
 
@@ -138,13 +137,13 @@ public class RegisterFragmentTest {
         ViewInteraction view4 = onView(withId(R.id.passwordET));
         view4.perform(typeText("aBC123xyZ!"));
         closeSoftKeyboard();
-        onView(withId(R.id.submitBtn)).perform(click());
+        onView(withId(R.id.submitBtn)).perform(scrollTo(), click());
         onView(withId(R.id.confirmNewPasswordET)).check(matches(hasErrorText(fieldError)));      //Correct error message was returned.
         //Second test is when the passwords do not match
         ViewInteraction view5 = onView(withId(R.id.confirmNewPasswordET));
         view5.perform(scrollTo(),typeText("aBC123xydZ!"));
         closeSoftKeyboard();
-        onView(withId(R.id.submitBtn)).perform(click());
+        onView(withId(R.id.submitBtn)).perform(scrollTo(), click());
         onView(withId(R.id.confirmNewPasswordET)).check(matches(hasErrorText("password doesn't match the above entered password!")));      //Correct error message was returned.
 
     }
