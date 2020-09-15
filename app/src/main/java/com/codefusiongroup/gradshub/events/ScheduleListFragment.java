ab7c1c278fa52911f30b253a7c681e55d30d8b31
bakang-dev <bakangmajova@gmail.com>
2020-09-15 12:54:56 +0200

    bug fixes for events schedule feature.

--- app/src/main/java/com/codefusiongroup/gradshub/events/ScheduleListFragment.java
+++ app/src/main/java/com/codefusiongroup/gradshub/events/ScheduleListFragment.java
@@ -25,11 +25,21 @@ import com.codefusiongroup.gradshub.common.GradsHubApplication;
 import com.codefusiongroup.gradshub.common.MainActivity;
 import com.codefusiongroup.gradshub.common.models.Schedule;
 import com.codefusiongroup.gradshub.common.models.User;
+import com.codefusiongroup.gradshub.common.network.ApiBaseResponse;
+import com.codefusiongroup.gradshub.common.network.ApiProvider;
+import com.codefusiongroup.gradshub.common.network.ApiResponseConstants;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
 
 
 public class ScheduleListFragment extends Fragment implements ScheduleContract.IScheduleView{
@@ -75,7 +85,7 @@ public class ScheduleListFragment extends Fragment implements ScheduleContract.I
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
+        Log.i(TAG, "onCreate executed");
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
 
@@ -93,6 +103,7 @@ public class ScheduleListFragment extends Fragment implements ScheduleContract.I
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.i(TAG, "onCreateView executed");
         mView = inflater.inflate(R.layout.fragment_schedule_item_list, container, false);
         return mView;
     }
@@ -100,16 +111,19 @@ public class ScheduleListFragment extends Fragment implements ScheduleContract.I
 
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-
+        Log.i(TAG, "onViewCreated executed");
         mPresenter.subscribe(this);
-        mPresenter.onViewCreated(mUser);
+
+        getEventsStars();
+        getUserFavouredEvents(mUser);
 
         onScheduleItemFavouredListener = (item) -> userCurrentlyFavouredEvents.add( item.getId() );
 
         onScheduleItemUnFavouredListener = (item) -> userUnFavouredEvents.add( item.getId() );
 
+
         // called after setting listeners like above
-        loadSchedule();
+        //mPresenter.onViewCreated(mUser);
 
         // listen for when user presses back button from the ScheduleListFragment and use that as an
         // indication that they are finished interacting with the events schedule and if they favoured or unfavoured
@@ -180,21 +194,6 @@ public class ScheduleListFragment extends Fragment implements ScheduleContract.I
     }
 
 
-    private void loadSchedule() {
-
-        if (mView instanceof RelativeLayout) {
-
-            Context context = mView.getContext();
-            RecyclerView recyclerView = mView.findViewById(R.id.scheduleList);
-            recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            mAdapter = new ScheduleListRecyclerViewAdapter(eventsSchedule, mListener, onScheduleItemFavouredListener, onScheduleItemUnFavouredListener);
-            recyclerView.setAdapter(mAdapter);
-
-        }
-
-    }
-
-
     // following methods called in ScheduleListRecyclerViewAdapter
     //====================================================================================
 
@@ -229,6 +228,7 @@ public class ScheduleListFragment extends Fragment implements ScheduleContract.I
         }
 
     }
+    //============================================================================================
 
 
     public static void removeEventFromFavouredList(String eventID) { userCurrentlyFavouredEvents.remove(eventID); }
@@ -236,37 +236,174 @@ public class ScheduleListFragment extends Fragment implements ScheduleContract.I
 
     public static void removeEventFromPreviouslyFavouredList(String eventID) { userPreviouslyFavouredEvents.remove(eventID); }
 
-    //=====================================================================================
+
+    public void getEventsStars() {
+
+        Log.i(TAG, "getEventsStars executed");
+        EventsAPI eventsAPI = ApiProvider.getEventsApiService();
+        eventsAPI.getEventsStars().enqueue(new Callback<JsonObject>() {
+
+            @Override
+            public void onResponse(Call<JsonObject> call, retrofit2.Response<JsonObject> response) {
+
+                if ( response.isSuccessful() ) {
+                    Log.i(TAG, "getEventsStars, response.isSuccessful() = true");
+
+                    JsonObject jsonObject = response.body();
+
+                    if ( jsonObject.get("success").getAsString().equals(ApiResponseConstants.API_SUCCESS_CODE) ) {
+
+                        Map<String, String> eventsStatistics = new HashMap<>();
+                        JsonArray favouredEventsJA = jsonObject.getAsJsonArray("message");
+
+                        for (JsonElement jsonElement: favouredEventsJA) {
+                            JsonObject favouredEventJO = jsonElement.getAsJsonObject();
+                            String eventID = favouredEventJO.get("EVENT_ID").getAsString();
+                            String eventStars = favouredEventJO.get("NO_STARS").getAsString();
+                            eventsStatistics.put(eventID, eventStars);
+                        }
+
+                        Log.i(TAG, "getEventsStars, eventsStats size --> "+eventsStatistics.size());
+                        for ( Map.Entry<String, String> entry: eventsStatistics.entrySet() ) {
+                            String eventID = entry.getKey();
+
+                            for (Schedule event: eventsSchedule) {
+                                // skip events that have already been assigned their star counts
+                                if ( event.getStarCount() == 0 && event.getId().equals(eventID) ) {
+                                    event.setStarCount( Integer.parseInt( entry.getValue() ) );
+                                    // break out of inner loop to prevent further checking of other events since we have
+                                    // already found an event with this ID.
+                                    break;
+                                }
+
+                            }
+
+                        }
+
+                    }
+
+                }
+
+                else {
+                    Log.i("ScheduleModel", "response.isSuccessful = false");
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                Log.i(TAG, "getEventsStars, onFailure executed");
+                GradsHubApplication.showToast("failed to update events stars.");
+                t.printStackTrace();
+            }
+
+        });
+
+    }
+
+
+    public void getUserFavouredEvents(User user) {
+
+        Log.i(TAG, "getUserFavouredEvents executed");
+        EventsAPI eventsAPI = ApiProvider.getEventsApiService();
+        HashMap<String, String> params = new HashMap<>();
+        params.put("user_id", user.getUserID());
+        eventsAPI.getUserFavouredEvents(params).enqueue(new Callback<JsonObject>() {
+
+            @Override
+            public void onResponse(Call<JsonObject> call, retrofit2.Response<JsonObject> response) {
+
+                if ( response.isSuccessful() ) {
+                    Log.i(TAG, "getUserFavouredEvents,  response.isSuccessful() = true");
+                    JsonObject jsonObject = response.body();
+
+                    if ( jsonObject.get("success").getAsString().equals(ApiResponseConstants.API_SUCCESS_CODE) ) {
+
+                        List<String> userFavouredEvents = new ArrayList<>();
+                        JsonArray favouredEventsJA = jsonObject.getAsJsonArray("message");
+
+                        for (JsonElement jsonElement: favouredEventsJA) {
+                            JsonObject favouredEventJO = jsonElement.getAsJsonObject();
+                            userFavouredEvents.add(favouredEventJO.get("EVENT_ID").getAsString());
+                        }
+
+                        userPreviouslyFavouredEvents.clear();
+                        userPreviouslyFavouredEvents.addAll(userFavouredEvents);
+                        Log.i(TAG, "getUserFavouredEvents,  userPreviouslyFavouredEvents size --> "+userPreviouslyFavouredEvents.size());
+
+
+                        for (Schedule event: eventsSchedule) {
+                            String event_id = event.getId();
+                            // search for the event id in the userFavouriteEvents list
+                            for (String eventID: userFavouredEvents) {
+
+                                if ( eventID.equals(event_id) ) {
+                                    event.setFavouredByUser(true);
+                                    // remove the processed event id from the userFavouredEvents list to reduce the size
+                                    // NOTE: this event id is removed after userPreviouslyFavouredEvents has been assigned
+                                    userFavouredEvents.remove(eventID);
+                                    // break out of inner loop to prevent further checking of other events since we have
+                                    // already found an event with this ID.
+                                    break;
+                                }
+
+                            }
+                        }
+
+
+                        if (mView instanceof RelativeLayout) {
+
+                            Context context = mView.getContext();
+                            RecyclerView recyclerView = mView.findViewById(R.id.scheduleList);
+                            recyclerView.setLayoutManager(new LinearLayoutManager(context));
+                            mAdapter = new ScheduleListRecyclerViewAdapter(eventsSchedule, mListener, onScheduleItemFavouredListener, onScheduleItemUnFavouredListener);
+                            recyclerView.setAdapter(mAdapter);
+
+                        }
+
+                    }
+                    else {
+                        // user has not favoured any events yet
+                        ApiBaseResponse apiDefault = new Gson().fromJson(jsonObject, ApiBaseResponse.class);
+                        GradsHubApplication.showToast( apiDefault.getStatusCode() );
+                    }
+                }
+                else {
+                    Log.i("ScheduleModel", "response.isSuccessful = false");
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                GradsHubApplication.showToast("failed to update your favourite events.");
+                t.printStackTrace();
+            }
+
+        });
+
+    }
+
+
+    //==============================================================================================
+    private void loadSchedule() {
+
+        if (mView instanceof RelativeLayout) {
+
+            Context context = mView.getContext();
+            RecyclerView recyclerView = mView.findViewById(R.id.scheduleList);
+            recyclerView.setLayoutManager(new LinearLayoutManager(context));
+            mAdapter = new ScheduleListRecyclerViewAdapter(eventsSchedule, mListener, onScheduleItemFavouredListener, onScheduleItemUnFavouredListener);
+            recyclerView.setAdapter(mAdapter);
+
+        }
+
+    }
 
 
     @Override
     public void updateEventsSchedule(List<String> userFavouriteEvents, Map<String, String> eventsStars) {
         //TODO: fix changes not showing immediately when user has favoured event
-
-        // set events that have been favoured by the user if there are any
-        if (userFavouriteEvents != null) {
-            userPreviouslyFavouredEvents.addAll(userFavouriteEvents);
-
-            for (Schedule event: eventsSchedule) {
-                String event_id = event.getId();
-                // search for the event id in the userFavouriteEvents list
-                for (String eventID: userFavouriteEvents) {
-
-                    if ( eventID.equals(event_id) ) {
-                        event.setFavouredByUser(true);
-                        // remove the processed event id from the userFavouredEvents list to reduce the size
-                        // NOTE: this event id is removed after userPreviouslyFavouredEvents has been assigned
-                        userFavouriteEvents.remove(eventID);
-                        // break out of inner loop to prevent further checking of other events since we have
-                        // already found an event with this ID.
-                        break;
-                    }
-
-                }
-            }
-
-        }
-
 
         // set star counts for each event if there are favoured events in the DB
         if (eventsStars != null) {
@@ -289,7 +426,52 @@ public class ScheduleListFragment extends Fragment implements ScheduleContract.I
         }
 
 
-        mAdapter.notifyDataSetChanged();
+        // set events that have been favoured by the user if there are any
+        if (userFavouriteEvents != null) {
+
+            userPreviouslyFavouredEvents.addAll(userFavouriteEvents);
+
+            for (Schedule event: eventsSchedule) {
+                String event_id = event.getId();
+                // search for the event id in the userFavouriteEvents list
+                for (String eventID: userFavouriteEvents) {
+
+                    if ( eventID.equals(event_id) ) {
+                        event.setFavouredByUser(true);
+                        // remove the processed event id from the userFavouredEvents list to reduce the size
+                        // NOTE: this event id is removed after userPreviouslyFavouredEvents has been assigned
+                        userFavouriteEvents.remove(eventID);
+                        // break out of inner loop to prevent further checking of other events since we have
+                        // already found an event with this ID.
+                        break;
+                    }
+
+                }
+            }
+
+            if (mView instanceof RelativeLayout) {
+
+                Context context = mView.getContext();
+                RecyclerView recyclerView = mView.findViewById(R.id.scheduleList);
+                recyclerView.setLayoutManager(new LinearLayoutManager(context));
+                if (mAdapter==null) {
+
+                    mAdapter = new ScheduleListRecyclerViewAdapter(eventsSchedule, mListener, onScheduleItemFavouredListener, onScheduleItemUnFavouredListener);
+                    Log.i(TAG, "updateEventsSchedule: (inside IF) mAdapter is null, creating adapter --> "+mAdapter);
+                }
+                recyclerView.setAdapter(mAdapter);
+                mAdapter.notifyDataSetChanged();
+                Log.i(TAG, "updateEventsSchedule: (inside) mAdapter not null --> "+mAdapter);
+                Log.i(TAG, "updateEventsSchedule: userPreviouslyFavouredEvents(inside) --> "+userPreviouslyFavouredEvents);
+
+            }
+
+            //mAdapter.notifyDataSetChanged();
+        }
+
+        //Log.i(TAG, "updateEventsSchedule: mAdapter (outside) --> "+mAdapter);
+        //Log.i(TAG, "updateEventsSchedule: userPreviouslyFavouredEvents(outside) --> "+userPreviouslyFavouredEvents);
+
     }
 
 
@@ -297,6 +479,7 @@ public class ScheduleListFragment extends Fragment implements ScheduleContract.I
     public void showUserFavouredEventsStatus(String message) {
         GradsHubApplication.showToast(message);
     }
+    //==============================================================================================
 
 
     @Override
@@ -315,6 +498,5 @@ public class ScheduleListFragment extends Fragment implements ScheduleContract.I
         super.onDetach();
         mListener = null;
     }
-
 
 }
