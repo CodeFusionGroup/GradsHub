5f865c623f965682cb2cce18ab7d21c8532b3452
BongumusaSizwe <1682836@students.wits.ac.za>
2020-10-07 15:57:19 +0200

    Test Update

--- app/src/test/java/com/codefusiongroup/gradshub/common/MainActivityTest.java
+++ app/src/test/java/com/codefusiongroup/gradshub/common/MainActivityTest.java
@@ -1,9 +1,16 @@
 package com.codefusiongroup.gradshub.common;
 
+import android.content.Context;
+import android.net.Uri;
+
+import com.codefusiongroup.gradshub.common.models.Schedule;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Date;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
@@ -35,6 +42,28 @@ public class MainActivityTest{
         String result = mockedMainActivity.validateEventLinkFormat(link2);
         assertThat(result, is("http://google.com"));
     }
+/*
+    Tests depends on alarm context. Impossible to unit test without device
+    @Test
+    public void  testScheduleNotification(){
+        MainActivity mockedMainActivity = new MainActivity();
+//        Context context, int notificationId, Schedule event, Date date, Uri uri
+        Context context = mockedMainActivity.getApplicationContext();
+        int notificationId = 10011101;          //Dummy notification
+        Schedule event = new Schedule();
+        event.setDate("10:10:2020");
+        event.setId("IdTest");
+        event.setFavouredByUser(true);
+        event.setPlace("University of the Witwatersrand");
+        event.setTitle("Test Event");
 
+           when(cont.getSystemService(Context.ALARM_SERVICE))
+                 .thenReturn("Something");
+        Date date = new Date();
+        //Uri uri = Uri.parse("http://stackoverflow.com");
+        Uri uri = null;
+        mockedMainActivity.scheduleNotification(context, notificationId, event, date, uri);
+    }
+*/
 
 }
